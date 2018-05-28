18fc3d8df28efd5e61705c37de9ba7d85ed1df53
unknown <hanli@pin.com>
2018-05-28 16:41:30 +0800

    first commit

---
+++ views.py
@@ -0,0 +1,82 @@
+"""
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from .models import Question, Choice
+from django.urls import reverse
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        select_choice.votes += 1
+        select_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+    """
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from polls.models import Question, Choice
+from django.utils import timezone
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ return the last five published questions"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailViews(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        select_choice.votes += 1
+        select_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
